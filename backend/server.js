ea01d8fc63e2f55d27865ba99b3e44582d0146e5
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2022-05-15 11:26:14 +0530

    Initiated Node Js. server for backend
    Node Js server app building with express js framework to write cruds.

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// Jai shreeram Jai devi Jaganmata
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((e) => e._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`port ${PORT} is running successfully`);
+});
